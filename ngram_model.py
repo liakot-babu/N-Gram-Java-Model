2182709a6cbb522774dbbb0bdd68f4d6f203a467
Mehedi Hasan Sun <46520339+mh-sun@users.noreply.github.com>
2024-09-26 23:42:12 -0400

    Added perplexity

--- ngram_model.py
+++ ngram_model.py
@@ -1,3 +1,4 @@
+import math
 import pickle
 import re
 import json
@@ -47,7 +48,7 @@ def save_tokens(data_path, token_path):
 def add_padding(datas, n):
     res = []
     for data in datas:
-        res.append('SOM ' * (n-1) + data + 'EOM')
+        res.append('SOM ' * (n-1) + data + ' EOM')
     return res
 
 
@@ -64,7 +65,7 @@ def save_ngram(data_path, ngram_path, n=2):
         print(f"{n}-gram data saved to {ngram_path}")
 
 
-def generate_ngram_prediction(test_data_path, token_path, ngram_path, n=2):
+def generate_ngram_prediction(test_data_path, token_path, ngram_path, gram_prob_path, n=2):
     with open(test_data_path, 'rb') as f:
         test_data = pickle.load(f)
 
@@ -75,6 +76,8 @@ def generate_ngram_prediction(test_data_path, token_path, ngram_path, n=2):
         tokens = pickle.load(f)
 
     accuracies = []
+
+    gram_prob_dict = {}
     for test_method in test_data[:10]:
         test_method_tokens = get_tokens(test_method)
         test_ngram_data = list(ngrams(test_method_tokens, n))
@@ -85,12 +88,17 @@ def generate_ngram_prediction(test_data_path, token_path, ngram_path, n=2):
             context = tuple(t_data[:-1])
             true_value = t_data[n-1]
 
-            prediction = get_prediction(ngram_data, context, tokens)
+            prediction, prob = get_prediction(ngram_data, context, tokens)
+
+            gram_prob_dict[t_data] = prob
 
             if prediction == true_value:
                 matching += 1
 
         accuracies.append(matching / len(test_method_tokens))
+
+    with open(gram_prob_path, 'wb') as f:
+        pickle.dump(gram_prob_dict, f)
 
     print(f"Mean Accuracy for {n}-Gram Model: {np.mean(accuracies)}")
 
@@ -113,9 +121,10 @@ def get_prediction(ngrams, context, tokens):
                 prediction = token
         else:
             prediction = "EOM"
+            max_prob = 0
             break
 
-    return prediction
+    return prediction, max_prob
 
 def load_methods(data_path):
     with open(data_path, 'r') as file:
@@ -148,16 +157,47 @@ def data_preprocess(data_path, train_data_path, test_data_path):
         pickle.dump(test_data, test_file)
 
 
+def calculate_perplexity(test_data_path, gram_prob_path, n):
+    with open(test_data_path, 'rb') as f:
+        test_corpus = pickle.load(f)
+
+    with open(gram_prob_path, 'rb') as f:
+        gram_prob = pickle.load(f)
+
+    total_log_prob = 0
+    total_tokens = 0
+    epsilon = 1e-10
+
+    for sentence in test_corpus:
+        sentence = 'SOM ' * (n - 1) + sentence + ' EOM'
+        sentence = get_tokens(sentence)
+        for i in range(n - 1, len(sentence)):
+            context = sentence[i - (n - 1):i]
+            token = sentence[i]
+            context.append(token)
+
+            prob = gram_prob[tuple(context)] if tuple(context) in gram_prob else 0
+
+            total_log_prob += -math.log(max(prob, epsilon))
+            total_tokens += 1
+
+    perplexity = math.exp(total_log_prob / total_tokens)
+
+    print(f'Perplexity: {perplexity}')
+
+
 if __name__ == "__main__":
     data_path = "final_method_data.json"
     train_data_path = "train_data.pkl"
     test_data_path = "test_data.pkl"
     token_path = "tokens.pkl"
     ngram_path = "ngrams.pkl"
+    gram_prob_path = "gram_probs.pkl"
 
     data_preprocess(data_path, train_data_path, test_data_path)
     save_tokens(train_data_path, token_path)
 
     for n in range(2, 6):
         save_ngram(train_data_path, ngram_path, n=n)
-        generate_ngram_prediction(test_data_path, token_path, ngram_path, n=n)
+        generate_ngram_prediction(test_data_path, token_path, ngram_path, gram_prob_path, n=n)
+        calculate_perplexity(test_data_path, gram_prob_path, n)
